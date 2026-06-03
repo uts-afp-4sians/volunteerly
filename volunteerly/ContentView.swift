9fb31b035de958f0f0dccc0bebc3fbb73a3d8775
gracefullight <gracefullight.dev@gmail.com>
2026-06-03 12:44:02 +1000

    Initial Commit

---
+++ volunteerly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  volunteerly
+//
+//  Created by Eunkwang Shin on 3/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ volunteerly/volunteerlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  volunteerlyApp.swift
+//  volunteerly
+//
+//  Created by Eunkwang Shin on 3/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct volunteerlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
